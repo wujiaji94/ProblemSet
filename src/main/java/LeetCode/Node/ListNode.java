86f007d760fb2166584aa2ea3f508cca63c0f75b
wujiaji <jiaji@stu.xmu.edu.cn>
2020-04-09 20:47:23 +0800

    solutions for LinkedList

---
+++ src/main/java/LeetCode/Node/ListNode.java
@@ -0,0 +1,15 @@
+package LeetCode.Node;
+
+
+/**
+ * @Author: wujiaji
+ * @Date: 2020-04-09 20:01
+ * @Description: Definition for singly-linked list.
+ **/
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+}
